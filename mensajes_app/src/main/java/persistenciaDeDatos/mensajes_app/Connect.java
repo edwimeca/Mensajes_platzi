5dbceaf74947bf90bbd86db3d77302961fa82c97
edwimeca <edwimeca@gmail.com>
2023-04-05 19:18:23 -0500

    proyecto finalizado

--- mensajes_app/src/main/java/persistenciaDeDatos/mensajes_app/Connect.java
+++ mensajes_app/src/main/java/persistenciaDeDatos/mensajes_app/Connect.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package qatesting.mensajes_app;
+package persistenciaDeDatos.mensajes_app;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -21,10 +21,6 @@ public class Connect {
         try {
             
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mensaje_app","root","Eleandro.2024");
-            if (connection != null) {
-                System.out.println("Connection OK");
-                    
-                }
             
         } catch (SQLException e) {
             System.out.println("There is not connection "+e);
--- mensajes_app/src/main/java/persistenciaDeDatos/mensajes_app/Messages_app.java
+++ mensajes_app/src/main/java/persistenciaDeDatos/mensajes_app/Messages_app.java
@@ -1,22 +1,52 @@
+package persistenciaDeDatos.mensajes_app;
 
 
-package qatesting.mensajes_app;
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.util.Scanner;
 
 /**
  *
  * @author edwimeca
  */
-public class Mensajes_app {
+public class Messages_app {
 
     public static void main(String[] args) {
-        
-        Connect connect = new Connect();
-        
-        Connection cnx = connect.get_connection();
-        
-       
-        
+
+        Scanner sc = new Scanner(System.in);
+        int option = 0;
+
+        do {
+            System.out.println("-------------");
+            System.out.println("Messages App");
+            System.out.println("1. Create Message");
+            System.out.println("2. Show Messages");
+            System.out.println("3. Delete Message");
+            System.out.println("4. Edit Message");
+            System.out.println("5. Exit");
+
+            //read the selected option
+            option = sc.nextInt();
+
+            switch (option) {
+                case 1:
+                    MessagesService.createMessage();
+                    break;
+                case 2:
+                    MessagesService.showMessages();
+                    break;
+                case 3:
+                    MessagesService.deleteMessage();
+                    break;
+                case 4:
+                    MessagesService.editMessage();
+
+                    break;
+                case 5:
+                    break;
+                default:
+                    break;
+            }
+
+        } while (option != 5);
+
     }
 }
